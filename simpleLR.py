2f4f7b065509aa9afba0bed79feceef673d7e704
khabboud <khabboud@users.noreply.github.com>
2017-06-19 15:03:40 -0400

    Update simpleLR.py

--- simpleLR.py
+++ simpleLR.py
@@ -3,39 +3,34 @@
 Created on Wed Sept 28 17:26 2016
 
 @author: khabboud 
-Implementing simple Linear Regression (LR): NG Andrea Course 
+Implementing simple Linear Regression (LR): NG Andrew Course 
 """
 
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
-#import pandas as pd 
 from matplotlib import cm
 import matplotlib.pyplot as plt
 import os  
 #======================Parameters========================================================
 ulfa=0.1;
 Theta=np.array([[0,0,0]])
-#Theta.shape += (1,)
-#Theta=Theta.T
 Iteration=50;
 accuracy=0.000001; # to check the difference between costs at consecutive iterations
 #==============================================================================
 path = os.getcwd() + '\LinearRegression-ex1\ex1\ex1data2.txt'  
-#data = pd.read_csv(path, header=None, names=['Profit', 'Population'])  
-#'\NYC collision DataSets\NYPD_Motor_Vehicle_Collisions.csv'
 data = np.genfromtxt(path, delimiter=',')
 #================Load raw data - One Dimentional Linear Regression ====================================
-#x=data[:,0] #Population
-#x.shape += (1,)
-#y=data[:,1]  #profit
-#y.shape += (1,)
-#plt.ylabel("Profit")
-#plt.xlabel("Population")
-#m= max(x.shape) # len(x) not necessarly true
-##colors = np.random.rand(m)
-##area = np.pi * (15 * np.random.rand(m))**2  # radius
-##plt.scatter(x, y, s=area, c=colors, alpha=0.5 )
-#plt.plot(x, y, 'rx')
+x=data[:,0] #Population
+x.shape += (1,)
+y=data[:,1]  #profit
+y.shape += (1,)
+plt.ylabel("Profit")
+plt.xlabel("Population")
+m= max(x.shape) # len(x) not necessarly true
+colors = np.random.rand(m)
+area = np.pi * (15 * np.random.rand(m))**2  # radius
+plt.scatter(x, y, s=area, c=colors, alpha=0.5 )
+plt.plot(x, y, 'rx')
 #============Load raw data - Multivariant Linear Regression
 xRaw=data[:,[0,1]] # size and number of bedrooms
 y=data[:,2]  #price of the house
@@ -55,7 +50,6 @@ ax.set_zlabel('Price')
 ax.set_zlim(0, np.max(y))
 #========Feature Scaling========================
 x=(xRaw-np.mean(xRaw, axis=0))/(np.sqrt(np.var(xRaw, axis=0)))
-#x=(xRaw-np.mean(xRaw, axis=0))/(np.max(xRaw, axis=0)-np.min(xRaw, axis=0))
 XRaw=np.concatenate((Ident, xRaw),axis=1)
 #======================Gradient Descent ==============================================
 def GD(ulfa,X,Y,theta,iteration,Accuracy):
@@ -75,8 +69,6 @@ def costFun(X,Y,theta):
     
     return Jx[0]
 X=np.concatenate((Ident, x),axis=1)
-
-#X=np.concatenate(([Ident.T], [x.T]), axis=0)
 
 j=costFun(X,y,Theta)
 [ThetaRes,Cost]=GD(ulfa,X,y,Theta,Iteration,accuracy)
